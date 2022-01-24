9159c8e3b6b92ba4a54deac94e895c31a9f0d2bc
BlendeaMihai <98150787+BlendeaMihai@users.noreply.github.com>
2022-01-24 14:21:15 +0100

    ciao

---
+++ BlendeaCiaoAPP/BlendeaCiaoAPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlendeaCiaoAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ciao!");
+        }
+    }
+}
